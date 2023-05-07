51b85f42b5ee8a6a9212931a9e06eb0baef99cfc
Bright Adams <brightadams58@gmail.com>
2023-05-07 08:48:35 +0000

    implement infinite scroll

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -26,7 +26,8 @@ const useGames = (gameQuery: GameQuery) => useInfiniteQuery<FetchResponse<Game>,
   }),
   getNextPageParam: (lastPage, allPages)=>{
     return lastPage.next? allPages.length + 1: undefined;
-  }
+  },
+  staleTime: 24 * 60 * 1000
 })
 
 export default useGames;
