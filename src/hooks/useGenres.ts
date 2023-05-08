a57d3a2ca5902193a4f3ac5207d4f58acc5fa287
Bright Adams <brightadams58@gmail.com>
2023-05-08 08:44:42 +0000

    refactoring interfaces

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -3,14 +3,9 @@ import genres from "../data/genres";
 import ms from "ms"
 import APIClient from "../services/api-client";
 import { FetchResponse } from "../services/api-client";
+import { Genre } from "../entities/Genre";
 
 const apiClient = new APIClient<Genre>("/genres")
-
-export interface Genre {
-  id: number;
-  name: string;
-  image_background: string;
-}
 
 const useGenres = () => useQuery({
   queryKey: ["genres"],
