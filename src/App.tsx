d499f622a55e3fe90e8249a60c344be94aa8d886
Bright Adams <brightadams58@gmail.com>
2023-05-08 08:10:05 +0000

    Setup routes

--- src/App.tsx
+++ src/App.tsx
@@ -8,37 +8,7 @@ import SortSelector from "./components/SortSelector";
 
 function App() {
   return (
-    <Grid
-      templateAreas={{
-        base: `"nav" "main"`,
-        lg: `"nav nav" "aside main"`,
-      }}
-      templateColumns={{
-        base: "1fr",
-        lg: "250px 1fr",
-      }}
-    >
-      <GridItem area="nav">
-        <NavBar />
-      </GridItem>
-      <Show above="lg">
-        <GridItem area="aside" paddingX={5}>
-          <GenreList />
-        </GridItem>
-      </Show>
-      <GridItem area="main">
-        <Box paddingLeft={2}>
-          <GameHeading />
-          <Flex marginBottom={5}>
-            <Box marginRight={5}>
-              <PlatformSelector />
-            </Box>
-            <SortSelector />
-          </Flex>
-        </Box>
-        <GameGrid />
-      </GridItem>
-    </Grid>
+    
   );
 }
 
